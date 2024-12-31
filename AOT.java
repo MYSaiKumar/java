d4fb3933e33723c8f283a09bfc61c10209969e3f
MySaiKumar <saimylavarapu10@gmail.com>
2024-12-31 10:54:34 +0530

    AOT modified

--- AOT.java
+++ AOT.java
@@ -5,10 +5,12 @@ public class AOT {
  public static void main(String ag[]) {
   double are,b,h;
   Scanner s = new Scanner(System.in);   
-  System.out.print("Enter radius of triangle:");
+  System.out.print("Enter values for base:");
   b = s.nextInt();
+  System.out.print("Enter value for height:");
   h = s.nextInt();
+
   are = (1.0/2)*b*h;   
-  System.out.println("area of trianglr" + are);
+  System.out.println("area of triangle :" + are);
  }
 }
